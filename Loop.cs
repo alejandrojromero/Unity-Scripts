7afb3b61d657e537806d30a86702f58b57e624c0
Alejandro Romero <alejandro_romero@brown.edu>
2019-05-05 17:26:06 -0400

    Loop 2D Backgrounds
    For a 2D side-scroller, loop between two backgrounds to create an infinite loop

---
+++ Loop.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loop : MonoBehaviour
+{
+
+    private BoxCollider2D collider;      
+    private float horizontalLength;      
+    private void Awake()
+    {
+        //Set collider of object
+        collider = GetComponent<BoxCollider2D>();
+        //Use collider length to determine loop timing
+        horizontalLength = collider.size.x;
+    }
+
+    //Shift the background at correct time
+    private void RepositionBackground()
+    {
+        //Transform the object to the offset position, which is 2 lengths away
+        //2 lengths away because we always use 2 backgrounds 
+        Vector2 offset = new Vector2(horizontalLength * 2f, 0);
+        transform.position = (Vector2)transform.position + offset;
+    }
+
+    //Update per frame. Shift if condition is met
+    private void Update()
+    {
+        if (transform.position.x < -horizontalLength)
+        {
+            //When the entire object has scrolled past the screen by its 
+            //length, reposition it
+            RepositionBackground();
+        }
+    }
+}
---
+++ Scroll.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    private Rigidbody2D body;
+    void Start()
+    {
+        //Set the object with this script to scroll at the predefined speed
+        body = GetComponent<Rigidbody2D>();
+        body.velocity = new Vector2(GameController.instance.scrollSpeed, 0);
+    }
+
+    void Update()
+    {
+        if (GameController.instance.gameOver == true)
+        {
+            //Stop scroll on game over
+            body.velocity = Vector2.zero;
+        }
+    }
+}
